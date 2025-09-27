21a9bbde517925d82d04b7968fb0e855648b663c
Wagner Tironi <profwagnertironi@gmail.com>
2025-09-26 21:16:01 -0300

    first commit

---
+++ Aula_11/app.js
@@ -0,0 +1,17 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+async function criarEPopularTabelaUsuarios(nome, sobrenome) {
+  const db = await open({
+    filename: "./banco.db",
+    driver: sqlite3.Database,
+  });
+  db.run(
+    'CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT)'
+  );
+  db.run('INSERT INTO usuarios (nome, sobrenome) VALUES (?,?)', [
+    nome,
+    sobrenome,
+  ]);
+}
+criarEPopularTabelaUsuarios("Daniel", "Porto");
